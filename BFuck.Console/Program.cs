77d29a6fc1651fbb7b77958b9c24b958a748bec1
ForNeVeR <neverthness@gmail.com>
2011-09-25 13:59:36 +0700

    BFuck.Console: console compiler frontend.

---
+++ BFuck.Console/Program.cs
@@ -0,0 +1,62 @@
+﻿/* Copyright (C) 2011 by ForNeVeR
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using System.IO;
+using BFuck.Compiler;
+
+namespace BFuck.Console
+{
+    /// <summary>
+    /// Main class for console compiler.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Entry point for console compiler.
+        /// </summary>
+        /// <param name="args">List of command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            if (args.Length < 1 || args[0] == @"--help")
+                PrintHelp();
+
+            foreach (var arg in args)
+            {
+                System.Console.Write("{0}... ", arg);
+                string programName = Path.GetFileNameWithoutExtension(arg);
+                string source = File.ReadAllText(arg);
+                Emitter.Compile(programName, source, string.Format(@"{0}.exe", programName));
+                System.Console.WriteLine("ok.");
+            }
+        }
+
+        private static void PrintHelp()
+        {
+            System.Console.WriteLine(@"BetterFuck Compiler console frontend.
+
+Usage:
+    BFuck.Console [arg1, arg2, ...]
+where [arg1, arg2, ...] - paths to source files. Resulting assemblies and
+modules will be saved in current directory with .exe and .mod extensions. Do
+not forget to distribute BFuck.Runtime.dll assembly with your assemblies!");
+        }
+    }
+}
