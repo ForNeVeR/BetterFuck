66c81f1b0d967ad70b9f7b41523f1afd3f846fb9
ForNeVeR <neverthness@gmail.com>
2011-10-01 15:49:10 +0700

    BFuck.Compiler.Emitter: added support for pbrain instructions to compiler.

--- BFuck.Compiler/Emitter.cs
+++ BFuck.Compiler/Emitter.cs
@@ -39,6 +39,11 @@ namespace BFuck.Compiler
         private const int DefaultMemorySize = 256;
 
         /// <summary>
+        /// All commands available for compiler.
+        /// </summary>
+        private static readonly char[] Commands = new[] {'+', '-', '>', '<', '.', ',', '[', ']', '(', ')'};
+
+        /// <summary>
         /// Compiles <paramref name="source"/> and prepares in-memory assembly.
         /// </summary>
         public static void Compile(string name, string source, string fileName)
@@ -58,7 +63,7 @@ namespace BFuck.Compiler
             var ilGenerator = methodBuilder.GetILGenerator();
 
             ProduceInitCode(ilGenerator);
-            ProduceCode(source, ilGenerator);
+            ProduceCode(source, typeBuilder, ilGenerator);
             ProduceFinalCode(ilGenerator);
 
             typeBuilder.CreateType();
@@ -78,7 +83,7 @@ namespace BFuck.Compiler
             {
                 foreach (var character in line)
                 {
-                    if (new[] { '+', '-', '>', '<', '.', ',', '[', ']' }.Contains(character))
+                    if (Commands.Contains(character))
                         result.Append(character);
                     else if (character == '#')
                         break;
@@ -117,8 +122,9 @@ namespace BFuck.Compiler
         /// Emits code of specified source fragment to specified generator.
         /// </summary>
         /// <param name="code">Brainfick source code.</param>
+        /// <param name="typeBuilder">Type builder for target assembly.</param>
         /// <param name="ilGenerator">IL generator.</param>
-        private static void ProduceCode(string code, ILGenerator ilGenerator)
+        private static void ProduceCode(string code, TypeBuilder typeBuilder, ILGenerator ilGenerator)
         {
             for (int i = 0; i < code.Length; ++i)
             {
@@ -126,44 +132,73 @@ namespace BFuck.Compiler
                 {
                     case '+':
                         ilGenerator.Emit(OpCodes.Ldloc_0);
-                        ilGenerator.EmitCall(OpCodes.Call, typeof(Engine).GetMethod("Add"), null);
+                        ilGenerator.EmitCall(OpCodes.Call, typeof (Engine).GetMethod("Add"), null);
                         break;
                     case '-':
                         ilGenerator.Emit(OpCodes.Ldloc_0);
-                        ilGenerator.EmitCall(OpCodes.Call, typeof(Engine).GetMethod("Dec"), null);
+                        ilGenerator.EmitCall(OpCodes.Call, typeof (Engine).GetMethod("Dec"), null);
                         break;
                     case '>':
                         ilGenerator.Emit(OpCodes.Ldloc_0);
-                        ilGenerator.EmitCall(OpCodes.Call, typeof(Engine).GetMethod("Forward"), null);
+                        ilGenerator.EmitCall(OpCodes.Call, typeof (Engine).GetMethod("Forward"), null);
                         break;
                     case '<':
                         ilGenerator.Emit(OpCodes.Ldloc_0);
-                        ilGenerator.EmitCall(OpCodes.Call, typeof(Engine).GetMethod("Back"), null);
+                        ilGenerator.EmitCall(OpCodes.Call, typeof (Engine).GetMethod("Back"), null);
                         break;
                     case '.':
                         ilGenerator.Emit(OpCodes.Ldloc_0);
-                        ilGenerator.EmitCall(OpCodes.Call, typeof(Engine).GetMethod("Out"), null);
+                        ilGenerator.EmitCall(OpCodes.Call, typeof (Engine).GetMethod("Out"), null);
                         break;
                     case ',':
                         ilGenerator.Emit(OpCodes.Ldloc_0);
-                        ilGenerator.EmitCall(OpCodes.Call, typeof(Engine).GetMethod("In"), null);
+                        ilGenerator.EmitCall(OpCodes.Call, typeof (Engine).GetMethod("In"), null);
                         break;
                     case '[':
                         var loopStartLabel = ilGenerator.DefineLabel();
                         ilGenerator.MarkLabel(loopStartLabel);
-                        int endLoop = FindClosingBrace(code, i);
-                        string innerBlock = code.Substring(i + 1, endLoop - i - 1);
-                        ProduceCode(innerBlock, ilGenerator);
+                        string innerBlock = GetCodeBlock(code, i);
+                        ProduceCode(innerBlock, typeBuilder, ilGenerator);
                         ilGenerator.Emit(OpCodes.Ldloc_0);
-                        ilGenerator.EmitCall(OpCodes.Call, typeof(Engine).GetMethod("Get"), null);
+                        ilGenerator.EmitCall(OpCodes.Call, typeof (Engine).GetMethod("Get"), null);
                         ilGenerator.Emit(OpCodes.Ldc_I4, 0);
                         ilGenerator.Emit(OpCodes.Bne_Un_S, loopStartLabel);
-                        i = endLoop;
+                        i += innerBlock.Length + 1;
+                        break;
+                    case '(':
+                        var procedureBody = GetCodeBlock(code, i);
+                        // TODO: Invent better naming scheme.
+                        var methodBuilder = typeBuilder.DefineMethod("procedure_" + procedureBody,
+                            MethodAttributes.Private | MethodAttributes.Static, typeof (void), new[] {typeof (Engine)});
+
+                        var procedureIlGenerator = methodBuilder.GetILGenerator();
+                        procedureIlGenerator.Emit(OpCodes.Ldarg_0);
+                        procedureIlGenerator.DeclareLocal(typeof(Engine));
+                        procedureIlGenerator.Emit(OpCodes.Stloc_0);
+                        ProduceCode(procedureBody, typeBuilder, procedureIlGenerator);
+
+                        ilGenerator.Emit(OpCodes.Ldloc_0);
+                        ilGenerator.EmitCall(OpCodes.Call, methodBuilder, null);
+
+                        i += procedureBody.Length + 1;
                         break;
                     default:
                         throw new NotSupportedException("Unsupported operation required.");
                 }
             }
+        }
+
+        /// <summary>
+        /// Extracts code block starting at index <paramref name="startIndex"/>.
+        /// </summary>
+        /// <param name="code">Code from which block will be extracted.</param>
+        /// <param name="startIndex">Index of code starting token (opening brace).</param>
+        /// <returns>Code block without braces.</returns>
+        private static string GetCodeBlock(string code, int startIndex)
+        {
+            int closingBraceIndex = FindClosingBrace(code, startIndex);
+            string innerBlock = code.Substring(startIndex + 1, closingBraceIndex - startIndex - 1);
+            return innerBlock;
         }
 
         /// <summary>
@@ -175,14 +210,24 @@ namespace BFuck.Compiler
         /// <returns></returns>
         private static int FindClosingBrace(string code, int index)
         {
-            if (code[index] != '[')
+            char openingBrace = code[index];
+            char closingBrace;
+            if (openingBrace == '[')
+            {
+                closingBrace = ']';
+            }
+            else if (openingBrace == '(')
+            {
+                closingBrace = ')';
+            }
+            else
                 throw new Exception("Not opened brace.");
 
             for (int i = index + 1; i < code.Length; ++i)
             {
-                if (code[i] == '[')
+                if (code[i] == openingBrace)
                     i = FindClosingBrace(code, i);
-                else if (code[i] == ']')
+                else if (code[i] == closingBrace)
                     return i;
             }
 
